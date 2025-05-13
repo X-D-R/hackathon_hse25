eee898d212ee7a48ae4fa4bf1c34b1764992c922
X-D-R <dabholak@gmail.com>
2025-05-13 17:44:31 +0300

    metrics included in class

--- prepocess_calculate/metrics.py
+++ prepocess_calculate/metrics.py
@@ -2,188 +2,113 @@ from typing import List
 
 import evaluate
 import numpy as np
-import pandas as pd
-from tqdm import tqdm
-
-rouge = evaluate.load("rouge")
-bleu = evaluate.load("bleu")
-chrf = evaluate.load("chrf")
-bertscore = evaluate.load("bertscore")
 
 
-def context_recall(ground_truth: str, contexts: List[str])->float:
-    """
-    Calc rouge btw contexts and ground truth.
-    Interpretation: ngram match (recall) btw contexts and desired answer.
+class MetricsCalculator:
+    def __init__(self):
+        self.rouge = evaluate.load("rouge")
+        self.bleu = evaluate.load("bleu")
+        self.chrf = evaluate.load("chrf")
+        self.bertscore = evaluate.load("bertscore")
 
-    ROUGE - https://huggingface.co/spaces/evaluate-metric/rouge
+    def context_recall(self, ground_truth: str, contexts: List[str]) -> float:
+        """
+        Calc rouge btw contexts and ground truth.
+        Interpretation: ngram match (recall) btw contexts and desired answer.
 
-    return: average rouge for all contexts.
-    """
-    rs = []
-    for c in contexts:
-        rs.append(
-            rouge.compute(
-                predictions=[str(c)],
-                references=[str(ground_truth)],
-            )["rouge2"]
-        )
+        ROUGE - https://huggingface.co/spaces/evaluate-metric/rouge
 
-    return np.mean(rs)
-
-
-def context_precision(ground_truth: str, contexts: List[str])->float:
-    """
-    Calc blue btw contexts and ground truth.
-    Interpretation: ngram match (precision) btw contexts and desired answer.
-
-    BLEU - https://aclanthology.org/P02-1040.pdf
-    max_order - max n-grams to count
-
-    return: average bleu (precision2, w/o brevity penalty) for all contexts.
-    """
-    bs = []
-    for c in contexts:
-
-        try:
-            bs.append(
-                bleu.compute(
+        return: average rouge for all contexts.
+        """
+        rs = []
+        for c in contexts:
+            rs.append(
+                self.rouge.compute(
                     predictions=[str(c)],
                     references=[str(ground_truth)],
-                    max_order=2,
-                )["precisions"][1]
+                )["rouge2"]
             )
-        except ZeroDivisionError:
-            bs.append(0)
 
-    return np.mean(bs)
+        return float(np.mean(rs))
 
-
-def answer_correctness_literal(
-    ground_truth: str,
-    answer: str,
-    char_order: int = 6,
-    word_order: int = 2,
-    beta: float = 1,
-)->float:
-    """
-    Calc chrF btw answer and ground truth.
-    Interpretation: lingustic match btw answer and desired answer.
-
-    chrF - https://aclanthology.org/W15-3049.pdf
-    char_order - n-gram length for chars, default is 6 (from the article)
-    word_order - n-gram length for words (chrF++), default is 2 (as it outperforms simple chrF)
-    beta - recall weight, beta=1 - simple F1-score
-
-    return: chrF for answ and gt.
-    """
-
-    score = chrf.compute(
-        predictions=[str(answer)],
-        references=[str(ground_truth)],
-        word_order=word_order,
-        char_order=char_order,
-        beta=beta,
-    )["score"]
-
-    return score
-
-
-def answer_correctness_neural(
-    ground_truth: str,
-    answer: str,
-    model_type: str = "cointegrated/rut5-base",
-)->float:
-    """
-    Calc bertscore btw answer and ground truth.
-    Interpretation: semantic cimilarity btw answer and desired answer.
-
-    BertScore - https://arxiv.org/pdf/1904.09675.pdf
-    model_type - embeds model  (default t5 as the best from my own research and experience)
-
-    return: bertscore-f1 for answ and gt.
-    """
-
-    score = bertscore.compute(
-        predictions=[str(answer)],
-        references=[str(ground_truth)],
-        batch_size=1,
-        model_type=model_type,
-        num_layers=11,
-    )["f1"]
-
-    return score[0]
-
-
-class ValidatorSimple:
-    """
-    Расчет простых метрик качества для заданного датасета.
-    """
-    def __init__(
-        self,
-        neural: bool = False,
-    ):
+    def context_precision(self, ground_truth: str, contexts: List[str]) -> float:
         """
-        param neural: есть гпу или нет. По дефолту ее нет(
-        """
-        self.neural = neural
+        Calc blue btw contexts and ground truth.
+        Interpretation: ngram match (precision) btw contexts and desired answer.
 
-    def score_sample(
-        self,
-        answer: str,
-        ground_truth: str,
-        context: List[str],
-    ):
+        BLEU - https://aclanthology.org/P02-1040.pdf
+        max_order - max n-grams to count
+
+        return: average bleu (precision2, w/o brevity penalty) for all contexts.
         """
-        Расчет для конкретного сэмпла в тестовом датасете.
-        """
-        scores = {}
-        scores["context_recall"] = [
-            context_recall(
-                ground_truth,
-                context,
-            )
-        ]
-        scores["context_precision"] = [
-            context_precision(
-                ground_truth,
-                context,
-            )
-        ]
-        scores["answer_correctness_literal"] = [
-            answer_correctness_literal(
-                ground_truth=ground_truth,
-                answer=answer,
-            )
-        ]
-        if self.neural:
-            scores["answer_correctness_neural"] = [
-                answer_correctness_neural(
-                    ground_truth=ground_truth,
-                    answer=answer,
+        bs = []
+        for c in contexts:
+
+            try:
+                bs.append(
+                    self.bleu.compute(
+                        predictions=[str(c)],
+                        references=[str(ground_truth)],
+                        max_order=2,
+                    )["precisions"][1]
                 )
-            ]
-        return scores
+            except ZeroDivisionError:
+                bs.append(0)
 
-    def validate_rag(
-        self,
-        test_set: pd.DataFrame,
-    ):
+        return float(np.mean(bs))
+
+    def answer_correctness_literal(
+            self,
+            ground_truth: str,
+            answer: str,
+            char_order: int = 6,
+            word_order: int = 2,
+            beta: float = 1,
+    ) -> float:
         """
-        param test_set: пандас датасет с нужными полями: answer, ground_truth, context, question
+        Calc chrF btw answer and ground truth.
+        Interpretation: lingustic match btw answer and desired answer.
+
+        chrF - https://aclanthology.org/W15-3049.pdf
+        char_order - n-gram length for chars, default is 6 (from the article)
+        word_order - n-gram length for words (chrF++), default is 2 (as it outperforms simple chrF)
+        beta - recall weight, beta=1 - simple F1-score
+
+        return: chrF for answ and gt.
         """
 
-        res = {}
-        for _, row in tqdm(test_set.iterrows(), "score_sample"):
-            gt = row.ground_truth
-            answer = row.answer
-            context = row.contexts
-            scores = self.score_sample(answer, gt, context)
-            if not res:
-                res = scores
-            else:
-                for k, v in scores.items():
-                    res[k].extend(v)
-        for k, v in res.items():
-            res[k] = np.mean(res[k])
-        return res
+        score = self.chrf.compute(
+            predictions=[str(answer)],
+            references=[str(ground_truth)],
+            word_order=word_order,
+            char_order=char_order,
+            beta=beta,
+        )["score"]
+
+        return score
+
+    def answer_correctness_neural(
+            self,
+            ground_truth: str,
+            answer: str,
+            model_type: str = "cointegrated/rut5-base",
+    ) -> float:
+        """
+        Calc bertscore btw answer and ground truth.
+        Interpretation: semantic cimilarity btw answer and desired answer.
+
+        BertScore - https://arxiv.org/pdf/1904.09675.pdf
+        model_type - embeds model  (default t5 as the best from my own research and experience)
+
+        return: bertscore-f1 for answ and gt.
+        """
+
+        score = self.bertscore.compute(
+            predictions=[str(answer)],
+            references=[str(ground_truth)],
+            batch_size=1,
+            model_type=model_type,
+            num_layers=11,
+        )["f1"]
+
+        return score[0]
