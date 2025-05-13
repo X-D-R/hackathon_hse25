cdd4559527ae067282abd50e459d39e988a48694
X-D-R <dabholak@gmail.com>
2025-05-13 17:28:59 +0300

    some updates for parsing

--- parsing_json.py
+++ parsing_json.py
@@ -1,8 +1,7 @@
 import json
 import re
-import ast
-from typing import List, Dict, Any
-import pandas as pd
+from typing import Dict, Any
+
 from prepocess_calculate.metrics import *
 
 
@@ -10,7 +9,8 @@ class LogsAnalyzer:
     def __init__(self):
         pass
 
-    def _clean_text(self, text: str) -> str:
+    @staticmethod
+    def _clean_text(text: str) -> str:
         """Очистка текста"""
         if not text:
             return ''
@@ -20,46 +20,60 @@ class LogsAnalyzer:
         """Парсинг всех данных"""
         return self._parse_data(file_path, include_time=True)
 
+    def parse_item(self, item: Dict, include_time: bool):
+        context = self._clean_text(" ".join(item['chat_history']['cleaned_contexts']))
+        answer = self._clean_text(item['chat_history']['old_answers'][0])
+        parsed = {
+            'selected_role': item['Выбранная роль'],
+            'campus': item['Кампус'],
+            'education_level': item['Уровень образования'],
+            'question_category': item['Категория вопроса'],
+            'user_question': self._clean_text(item['chat_history']['old_questions'][0]),
+            'user_filters': item['user_filters'],
+            'question_filters': item['question_filters'],
+            'answer': answer,
+            'user_mark': 1 if item['Оценка пользователя'] == "+" else -1 if item['Оценка пользователя'] == "-" else 0,
+            'contexts': context,
+            'answer_correctness_literal': answer_correctness_literal(context, answer),
+            'answer_correctness_neural': answer_correctness_neural(context, answer)
+        }
+
+        if include_time:
+            parsed.update({
+                'response_time': item['Время ответа модели']
+            })
+
+        return parsed
+
     def _parse_data(self, file_path: str, include_time: bool) -> List[Dict[str, Any]]:
         """Базовая функция парсинга"""
         try:
             with open(file_path, 'r', encoding='utf-8') as f:
                 data = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError) as e:
-            # logger.error(f"Error loading file {file_path}: {str(e)}")
             raise
 
         result = []
         for item in data:
-            context = self._clean_text(" ".join(item['chat_history']['cleaned_contexts']))
-            answer = self._clean_text(item['chat_history']['old_answers'][0])
-            parsed = {
-                'selected_role': item['Выбранная роль'],
-                'campus': item['Кампус'],
-                'education_level': item['Уровень образования'],
-                'question_category': item['Категория вопроса'],
-                'user_question': self._clean_text(item['chat_history']['old_questions'][0]),
-                'user_filters': item['user_filters'],
-                'question_filters': item['question_filters'],
-                'answer': answer,
-                'user_mark': 1 if item['Оценка пользователя'] == "+" else -1 if item['Оценка пользователя'] == "-" else 0,
-                'contexts': context,
-                'answer_correctness_literal': answer_correctness_literal(context, answer),
-                'answer_correctness_neural': answer_correctness_neural(context, answer)
-            }
-
-            if include_time:
-                parsed.update({
-                    'response_time': item['Время ответа модели']
-                })
-
-            result.append(parsed)
+            result.append(self.parse_item(item, include_time))
 
         return result
+
+    def export_data(self, data: List[Dict[str, Any]], output_name: str = "result", extension: str = "json") -> None:
+        """Экспорт данных в различные форматы"""
+        df = pd.DataFrame(data)
+
+        if extension == "csv":
+            df.to_csv(f"{output_name}.csv", index=False)
+        elif extension == "xlsx":
+            df.to_excel(f"{output_name}.xlsx", index=False)
+        elif extension == "json":
+            df.to_json(f"{output_name}.json", index=False)
+        else:
+            print("Unsupported format")
 
 
 if __name__ == "__main__":
     log_obj = LogsAnalyzer()
-    df = pd.DataFrame(log_obj.parse_all_data("logs/new_logs.json"))
-    # df.to_csv('result.csv')
-    df.to_excel('result.xlsx')
+    data = log_obj.parse_all_data("logs/new_logs.json")
+    log_obj.export_data(data, extension="xlsx")
