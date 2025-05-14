f5fbda08a1b59b5f208b5d77467ddfd5f7320ca2
X-D-R <dabholak@gmail.com>
2025-05-14 15:17:00 +0300

    Solve bug with wrong save to json, founded only one optimization - regular expression, but it is not significant

--- parsing_json.py
+++ parsing_json.py
@@ -9,14 +9,13 @@ from profiling import Profiler
 
 class LogsAnalyzer:
     def __init__(self):
-        pass
+        self.clean_text = re.compile(r'\\[nrt]|[\n\r\t]|\s+')
 
-    @staticmethod
-    def _clean_text(text: str) -> str:
+    def _clean_text(self, text: str) -> str:
         """Очистка текста"""
         if not text:
             return ''
-        return re.sub(r'\\[nrt]|[\n\r\t]+|\s+', ' ', text).strip()
+        return self.clean_text.sub(' ', text).strip()
 
     def parse_all_data(self, file_path: str) -> List[Dict[str, Any]]:
         """Парсинг всех данных"""
@@ -65,13 +64,12 @@ class LogsAnalyzer:
     def export_data(self, data: List[Dict[str, Any]], output_name: str = "result", extension: str = "json") -> None:
         """Экспорт данных в различные форматы"""
         df = pd.DataFrame(data)
-
         if extension == "csv":
             df.to_csv(f"{output_name}.csv", index=False)
         elif extension == "xlsx":
             df.to_excel(f"{output_name}.xlsx", index=False)
         elif extension == "json":
-            df.to_json(f"{output_name}.json", index=False)
+            df.to_json(f"{output_name}.json", orient='records', indent=2, force_ascii=False)
         else:
             print("Unsupported format")
 
@@ -79,9 +77,37 @@ class LogsAnalyzer:
 def main():
     log_obj = LogsAnalyzer()
     data = log_obj.parse_all_data("logs/new_logs.json")
-    log_obj.export_data(data, output_name="result", extension="xlsx")
+    log_obj.export_data(data, output_name="result", extension="json")
+
+
+def check_export(data: List[Dict[str, Any]]):
+    log_obj = LogsAnalyzer()
+    log_obj.export_data(data, output_name="result", extension="json")
+
+
+def check_clean_text():
+    log_obj = LogsAnalyzer()
+    data = log_obj._clean_text("студентов равно двум — это значит, что у студента две задолженности, и он допускается "
+                               "к пересдачам, но если получит третью неудовлетворительную оценку, то будет отчислен. "
+                               "Критическое значение КУД (качество учебной деятельности) для студентов, относящихся к "
+                               "отдельным категориям равно пяти , если превышение обычного КУД (качество учебной "
+                               "деятельности) в отношении студента зафиксировано в первый раз . Студенты, "
+                               "КУД (качество учебной деятельности) которых превышает критическое значение (с учетом "
+                               "отнесения студента к отдельной категории и наличия или отсутствия повтора критической "
+                               "ситуации), подлежат отчислению как не выполняющие обязанности по добросовестному "
+                               "освоению образовательной программы и выполнению учебного плана. Подробнее об "
+                               "отдельных категориях студентов . Пример расчета обычного КУД (качество учебной "
+                               "деятельности) : КУД=0 — у студента нет задолженностей, все хорошо КУД=2 — у студента "
+                               "две задолженности, он допускается к пересдачам КУД=4 — у студента четыре "
+                               "задолженности, он подлежит отчислению Документы Положение об организации "
+                               "промежуточной аттестации и текущего контроля успеваемости студентов НИУ (Национальный "
+                               "исследовательский университет) ВШЭ (Высшая школа экономики) (ПО")
 
 
 if __name__ == "__main__":
     profiler = Profiler(output_dir="profiling_results")
     profiler.run_with_profiling(main)
+    # timer = timeit.Timer(lambda: main())
+    # times = timer.repeat(repeat=1000, number=1)
+    # print(f"Overall time: {sum(times)}")
+    # print(f"Mean time: {sum(times) / len(times)}")
