424332d5685c1ee65504a4ff6e6d0ed51434e744
Denis <tankt34.stepanoff@yandex.ru>
2025-07-28 16:18:19 +0300

    norm navigation

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import streamlit as st
 page_dict = {}
 page_dict["Навигация"] = [
     st.Page("pages/General.py", title="Общий обзор", icon="🌍"),
-    st.Page("pages/Errors.py", title="Ошибки", icon="🚨"),
-    st.Page("pages/Old_dash.py", title="Старый дашборд", icon="📊")
+    st.Page("pages/Errors.py", title="Ошибки", icon="🚨")
 ]
+page_dict['Для разработчиков'] = [st.Page("pages/Old_dash.py", title="Вся статистика", icon="📊")]
 
 pg = st.navigation(page_dict)
 pg.run()
