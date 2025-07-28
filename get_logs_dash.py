181a3e165a6f807ac59fa6929a57b5e4fd83d5ca
Denis <tankt34.stepanoff@yandex.ru>
2025-07-28 15:16:05 +0300

    Egor give us

---
+++ get_logs_dash.py
@@ -0,0 +1,18 @@
+import requests
+
+url = "http://46.229.141.86:44455/get_logs"
+
+# Указываем название файла который нам нужен
+search_request = {
+    "log_name": "actual_july_logs.json",
+}
+
+
+response = requests.post(url, json=search_request)
+
+if response.status_code == 200:
+    data = response.json()
+    # Сами логи
+    print(data["logs"])
+    # Время последнего обновления
+    print(data["time"])
