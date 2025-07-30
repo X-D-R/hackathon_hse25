720cc1df38dfab9f6d1a6aea18a6e134a0eaa721
Denis <tankt34.stepanoff@yandex.ru>
2025-07-30 11:31:07 +0300

    New start

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
+import os
+
 import streamlit as st
 
-page_dict = {}
-page_dict["Навигация"] = [
-    st.Page("pages/General.py", title="Общий обзор", icon="🌍"),
-    st.Page("pages/Errors.py", title="Ошибки", icon="🚨")
-]
-page_dict['Для разработчиков'] = [st.Page("pages/Old_dash.py", title="Вся статистика", icon="📊")]
+from parsing import pipeline
 
-pg = st.navigation(page_dict)
-pg.run()
+REQUIRED_FILES = [
+    "data/result.json",
+    "data/parsed_data_cache.json",
+    "data/log3.json"
+]
+
+
+def show_loader():
+    st.set_page_config(page_title="Загрузка", layout="wide")
+    with st.spinner("Загружаю данные..."):
+        pipeline()
+    st.success("Готово! Перезапусти страницу ⟳")
+    st.stop()
+
+
+def start():
+    if not all(os.path.exists(f) for f in REQUIRED_FILES):
+        show_loader()
+
+    page_dict = {
+        "Навигация": [
+            st.Page("app_pages/General.py", title="Общий обзор", icon="🌍"),
+            st.Page("app_pages/Errors.py", title="Ошибки", icon="🚨")
+        ],
+        "Для разработчиков": [
+            st.Page("app_pages/Old_dash.py", title="Вся статистика", icon="📊")
+        ]
+    }
+    pg = st.navigation(page_dict)
+    pg.run()
+
+
+if __name__ == "__main__":
+    start()
