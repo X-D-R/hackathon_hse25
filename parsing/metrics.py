5853d90767acf9cff170b17dc0e32e00594f474a
Denis <tankt34.stepanoff@yandex.ru>
2025-07-31 17:31:57 +0300

    gives what programm want

--- parsing/metrics.py
+++ parsing/metrics.py
@@ -16,7 +16,7 @@ class MetricsCalculator:
         self.bertscore = evaluate.load("bertscore")
         self.faithfulness_model = pipeline("text-classification",
                                            model="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli",
-                                           return_all_scores=True,
+                                           top_k=None,
                                            device=-1)
         self.relevance_model = SentenceTransformer("all-MiniLM-L6-v2")
 
